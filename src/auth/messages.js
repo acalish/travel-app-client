9cea0fa03bb14369169155ae01e56ef756495875
acalish <acalish@gmail.com>
2018-10-06 16:15:48 -0400

    Update auth messaging
    - edit auth success / failure messages

--- src/auth/messages.js
+++ src/auth/messages.js
@@ -1,10 +1,9 @@
 export default {
   signUpSuccess: 'Succesfully registered! You\'ve been signed in as well.',
-  signUpFailure: 'Registration failed. Email may be taked, or passwords don\'t'
+  signUpFailure: 'Registration failed. Email may be taken, or passwords don\'t'
                   + ' match.',
   signInSuccess: 'Welcome!',
-  signInFailure: 'Failed to sign in. Check your email and password and try' +
-                  ' again.',
+  signInFailure: 'Failed to sign in. Please check that your email and password are correct.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
   changePasswordFailure: 'Failed to change passwords. Check your old password' +
