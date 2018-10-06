c5a2499dbbd50698a63f9b51934da65f8354da5a
acalish <acalish@gmail.com>
2018-10-06 16:04:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my travel app client repo.
---
+++ src/apiConfig.js
@@ -0,0 +1,13 @@
+let apiUrl
+const apiUrls = {
+  production: 'https://floating-chamber-13025.herokuapp.com/',
+  development: 'http://localhost:4741'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+export default apiUrl
