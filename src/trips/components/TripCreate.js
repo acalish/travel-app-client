b8c9460396626316d8053d79bef07503a3fbd2cf
acalish <acalish@gmail.com>
2018-10-10 11:03:26 -0400

    Update create trip to reroute to /trips
    - when a user creates a trip it will now reroute to /trips with their
    full trip listings

--- src/trips/components/TripCreate.js
+++ src/trips/components/TripCreate.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import { handleErrors, createTrip } from '../api'
 import apiUrl from '../../apiConfig'
-import TripForm from './TripForm'
 
 class TripCreate extends Component {
   constructor() {
@@ -23,39 +23,37 @@ createTrip = event => {
   event.preventDefault()
 
   const { name, destination, startDate, endDate } = this.state
-  const { user } = this.props
+  const { user, history } = this.props
 
   createTrip(this.state, user)
-    .then((res) => {console.log(res)
-      this.setState({})
-    })
-    .catch(console.log)
-    // .then(console.log(this.state, 'trip info'))
-    // .then(console.log(this.props, 'user'))
+    .then(handleErrors)
+    .then(() => history.push('/trips'))
 }
 
 render() {
   const { trip } = this.state
 
   return (
+    <React.Fragment>
+      <h2> Create a Trip Listing </h2>
+      <form className='trip-form' onSubmit={this.createTrip}>
+        <label>Name: </label>
+        <input name="name" onChange={this.handleChange} required placeholder="name" type="text" value={this.state.name} />
 
-    <form className='trip-form' onSubmit={this.createTrip}>
-      <label>Name: </label>
-      <input name="name" onChange={this.handleChange} required placeholder="name" type="text" value={this.state.name} />
+        <label>Destination: </label>
+        <input name="destination" onChange={this.handleChange} required placeholder="destination" type="text" value={this.state.destination} />
 
-      <label>Destination: </label>
-      <input name="destination" onChange={this.handleChange} required placeholder="destination" type="text" value={this.state.destination} />
+        <label>Start Date: </label>
+        <input name="startDate" onChange={this.handleChange} required placeholder="start date" type="date" value={this.state.startDate} />
 
-      <label>Start Date: </label>
-      <input name="startDate" onChange={this.handleChange} required placeholder="start date" type="date" value={this.state.startDate} />
+        <label>End Date: </label>
+        <input name="endDate" onChange={this.handleChange} required placedholder="end date" type="date" value={this.state.endDate} />
 
-      <label>End Date: </label>
-      <input name="endDate" onChange={this.handleChange} required placedholder="end date" type="date" value={this.state.endDate} />
-
-      <button type="submit">Create</button>
-    </form>
+        <button type="submit">Create</button>
+      </form>
+    </React.Fragment>
   )
 }
 }
 
-export default TripCreate
+export default withRouter(TripCreate)
