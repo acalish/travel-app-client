0a7de93df595e893ccf5170ca46478f0860cabff
acalish <acalish@gmail.com>
2018-10-11 15:14:39 -0400

    Add color scheme
    - define color scheme and add it to start styling

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,29 +5,29 @@ import './Header.scss'
 
 const authenticatedOptions = (
   <React.Fragment>
-    <Link to="/trips">Trips</Link>
-    <Link to="/trips-create">Create</Link>
-    <Link to="/change-password">Change Password</Link>
-    <Link to="/sign-out">Sign Out</Link>
+    <Link className="nav-link" to="/trips">Trips</Link>
+    <Link className="nav-link" to="/trips-create">Create</Link>
+    <Link className="nav-link" to="/change-password">Change Password</Link>
+    <Link className="nav-link" to="/sign-out">Sign Out</Link>
   </React.Fragment>
 )
 
 const unauthenticatedOptions = (
   <React.Fragment>
-    <Link to="/sign-up">Sign Up</Link>
-    <Link to="/sign-in">Sign In</Link>
+    <Link className="nav-link" to="/sign-up">Sign Up</Link>
+    <Link className="nav-link" to="/sign-in">Sign In</Link>
   </React.Fragment>
 )
 
 const alwaysOptions = (
   <React.Fragment>
-    <Link to="/">Home</Link>
+    <Link className="nav-link" to="/">Home</Link>
   </React.Fragment>
 )
 
 const Header = ({ user }) => (
   <header className="main-header">
-    <h1>Go There, Do This!</h1>
+    <h1 className="brand">Go There, Do This!</h1>
     <nav>
       { user && <span>Welcome, {user.email}</span>}
       { user ? authenticatedOptions : unauthenticatedOptions }
--- src/trips/components/TripIndex.js
+++ src/trips/components/TripIndex.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { handleErrors, createTrip, indexTrip, deleteTrip } from '../api'
 import apiUrl from '../../apiConfig'
 import { Card, CardBody, CardTitle } from 'mdbreact'
+import './Trip.scss'
 
 class TripIndex extends React.Component {
   constructor(props) {
@@ -26,29 +27,21 @@ class TripIndex extends React.Component {
     this.setState({trips: this.state.trips.filter(trip => trip.id !== tripId)})
   }
 
-  // <div key={trip.id}>
-  //   <p>{trip.name}</p>
-  //   <p>{trip.destination}</p>
-  //   <p>{trip.start_date}</p>
-  //   <p>{trip.end_date}</p>
-  //   <p><Link to={`/trips/${trip.id}/update`}>update</Link> | <a href="" onClick={(event) => this.deleteTrip(event, trip.id)}>delete</a></p>
-  // </div>
-
   render() {
     const tripListing = this.state.trips.map(trip => {
       return (
-        <Card key={trip.id}>
+        <Card className="trip-card" key={trip.id}>
           <CardBody>
             <CardTitle>{trip.name}</CardTitle>
             <p>{trip.destination}</p>
             <p>start date: {trip.start_date}</p>
             <p>end date: {trip.end_date}</p>
-            <p><Link to={{
+            <p><Link className="link" to={{
               pathname: `/trips/${trip.id}/update`,
               state: {tripName: trip.name,
                 tripDestination: trip.destination,
                 tripStartDate: trip.start_date}
-            }}>update</Link> | <a href="" onClick={(event) => this.deleteTrip(event, trip.id)}>delete</a></p>
+            }}> update</Link>  | <a className="link" href="" onClick={(event) => this.deleteTrip(event, trip.id)}> delete </a></p>
           </CardBody>
         </Card>
       )
@@ -58,7 +51,7 @@ class TripIndex extends React.Component {
       <React.Fragment>
         <h1>Trips</h1>
 
-        <div>
+        <div className='card-parent'>
           {tripListing}
         </div>
       </React.Fragment>
